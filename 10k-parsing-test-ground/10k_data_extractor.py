d329b0c0abf5bfc3c09098ca523ab8f4a057b5a0
Nolan Gagnon <nolan.gagnon@maine.edu>
2017-12-23 21:10:05 -0400

    extracting data from EDGAR

--- 10k-parsing-test-ground/10k_data_extractor.py
+++ 10k-parsing-test-ground/10k_data_extractor.py
@@ -9,8 +9,14 @@ from bs4 import BeautifulSoup
 import requests
 import re ## Regular expressions
 import pandas as pd
+from pprint import pprint
 
 
+"""**************************************************
+*                                                   *
+*                       CLASSES                     *
+*                                                   *
+**************************************************"""
 class Form10K_Data_Extractor:
 	
 	'''Class for extracting financial data from a company's annual report (i.e., Form 10K)'''
@@ -23,11 +29,35 @@ class Form10K_Data_Extractor:
 		self._ticker = ticker	
 		self.forms = []
 		self.net_incomes = []
+		self.f10k_links = []
 	
 	###################################
 	## Private methods
 	###################################
 
+	def __compile_f10k_links(self):
+		
+		'''Creates a list of links to a company's 10K forms'''
+
+		webpage = "https://www.sec.gov/cgi-bin/browse-edgar?CIK=NVDA&owner=exclude&action=getcompany"
+		soup = BeautifulSoup(requests.get(webpage).content, "html5lib")
+
+		table_rows = soup.findAll("tr")
+
+		link_base = "https://www.sec.gov"
+
+		for tr in table_rows:
+			tr_children = tr.findChildren()
+			for c in tr_children:
+				if c.text == "10-K":
+					link = link_base + c.findNext("td").findChildren()[0]['href']
+					self.f10k_links.append(link)
+	
+	def test(self):
+		self.__compile_f10k_links()
+		print(self.f10k_links)
+
+	
 	def __EDGAR_retrieve(self):
 
 		'''Retrieves all 10k forms from the SEC's EDGAR database'''
@@ -71,37 +101,5 @@ class Form10K_Data_Extractor:
 		print(self.net_incomes)	
 
 		
-
 F = Form10K_Data_Extractor("NVDA")
-F.print_net_incomes()
-'''
-webpage = open("nvda_2017_10k.html").read()
-soup = BeautifulSoup(webpage, "html5lib")
-
-trs = soup.findAll("tr")
-
-trs_text = ""
-
-for tr in trs:
-	trs_text += tr.text + "\n"
-
-ni_regex = re.compile(r"(Net income((\$(\d+,*)+)(\s|\xa0)*)+\n)")
-
-text = open("doc.txt").read()
-
-matches = ni_regex.findall(trs_text)
-
-# For now, assume the correct match is the first one
-
-ni_line = matches[0][0].replace("\xa0", " ").replace("\n", "")
-
-ni_data = ni_line.split("$")
-ni_data.pop(0)
-
-for i, e in enumerate(ni_data):
-	if "," in e:
-		e = e.replace(",", "")
-	ni_data[i] = e.strip()	
-
-print(ni_data)
-'''
+F.test()
